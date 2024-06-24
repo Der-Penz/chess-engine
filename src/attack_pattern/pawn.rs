1da8f936442f428e11139c1698d6b38012e4da62
DerPenz <mike2407@t-online.de>
2024-06-24 17:55:56 +0200

    fixed pawn attack pattern to include double pawn move on starting row

--- src/attack_pattern/pawn.rs
+++ src/attack_pattern/pawn.rs
@@ -5,13 +5,20 @@ const fn calculate_pawn_attack_pattern(white: bool) -> [u64; 64] {
         let pos = 1 << i;
         if white{
             res[i] = pos << 8;
+            if i / 8 == 1{
+                res[i] |= pos << 16;
+            }
         }else{
             res[i] = pos >> 8;
+            if i / 8 == 6{
+                res[i] |= pos >> 16;
+            }
         }
         i += 1;
     }
     res
 }
 
-pub const ATTACK_PATTERN_PAWN_WHITE: [u64; 64] = calculate_pawn_attack_pattern(true);
-pub const ATTACK_PATTERN_PAWN_BLACK: [u64; 64] = calculate_pawn_attack_pattern(false);
+const ATTACK_PATTERN_PAWN_WHITE: [u64; 64] = calculate_pawn_attack_pattern(true);
+const ATTACK_PATTERN_PAWN_BLACK: [u64; 64] = calculate_pawn_attack_pattern(false);
+pub const ATTACK_PATTERN_PAWN: [[u64; 64]; 2] = [ATTACK_PATTERN_PAWN_WHITE, ATTACK_PATTERN_PAWN_BLACK];
