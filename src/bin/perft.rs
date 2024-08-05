ec2f777d95f96fb4a749b526e57ca797a318ec48
DerPenz <mike2407@t-online.de>
2024-08-05 15:52:13 +0200

    comment out perft for now

--- src/bin/perft.rs
+++ src/bin/perft.rs
@@ -1,46 +1,50 @@
 use std::vec;
 
-use chess_bot::game::{board::move_gen::MoveGeneration, Board, Move};
+use chess_bot::{
+    game::{board::move_gen::MoveGeneration, Board, Move},
+    init_logging,
+};
 
 fn main() {
+    init_logging();
     //read args depth and fen from command line
     //validate if depth must be provided fen is optional and default is startpos
-    let args: Vec<String> = std::env::args().collect();
+    // let args: Vec<String> = std::env::args().collect();
 
-    let depth = args
-        .get(1)
-        .expect("Depth must be provided")
-        .parse::<u8>()
-        .expect("Depth must be a number");
+    // let depth = args
+    //     .get(1)
+    //     .expect("Depth must be provided")
+    //     .parse::<u8>()
+    //     .expect("Depth must be a number");
 
-    let mut board = if let Some(fen) = args.get(2) {
-        println!("FEN provided: {}", fen);
-        Board::from_fen(fen).expect("Invalid FEN")
-    } else {
-        Board::default()
-    };
+    // let mut board = if let Some(fen) = args.get(2) {
+    //     println!("FEN provided: {}", fen);
+    //     Board::from_fen(fen).expect("Invalid FEN")
+    // } else {
+    //     Board::default()
+    // };
 
-    println!(
-        "Perft result with depth {} for FEN: {}",
-        depth,
-        board.to_fen()
-    );
+    // println!(
+    //     "Perft result with depth {} for FEN: {}",
+    //     depth,
+    //     board.to_fen()
+    // );
 
-    let res = perft(depth, &mut board, false);
+    // let res = perft(depth, &mut board);
 
-    for (mov, counter) in res.iter() {
-        let mut mov_str = String::with_capacity(30);
-        mov_str.push_str(format!("{}", mov).as_str());
-        if mov_str.len() < 30 {
-            mov_str.push_str(&" ".repeat(30 - mov_str.len()));
-        }
-        println!("{} - {}", mov_str, counter.count);
-        // println!("{} - {}", mov_str, counter);
-    }
-    println!(
-        "Total nodes: {}",
-        res.iter().map(|(_, c)| c.count).sum::<usize>()
-    );
+    // for (mov, counter) in res.iter() {
+    //     let mut mov_str = String::with_capacity(30);
+    //     mov_str.push_str(format!("{}", mov).as_str());
+    //     if mov_str.len() < 30 {
+    //         mov_str.push_str(&" ".repeat(30 - mov_str.len()));
+    //     }
+    //     println!("{} - {}", mov_str, counter.count);
+    //     // println!("{} - {}", mov_str, counter);
+    // }
+    // println!(
+    //     "Total nodes: {}",
+    //     res.iter().map(|(_, c)| c.count).sum::<usize>()
+    // );
 
     // for depth in 0..=depth {
     //     let moves_at_depth = perft_depth(depth, &mut board);
@@ -48,64 +52,64 @@ fn main() {
     // }
 }
 
-fn perft(depth: u8, board: &mut Board, simple: bool) -> Vec<(Move, MoveCounter)> {
-    if depth == 0 {
-        return vec![];
-    }
-    let mut results = Vec::new();
-    let moves = MoveGeneration::generate_all_moves(&board);
-    for idx in 0..moves.len() {
-        let mut counter = MoveCounter::default();
-        let mov = moves[idx];
-        board.make_move(&mov, false, false).expect("Valid move");
-        counter += perft_depth(depth - 1, board);
-        match mov.flag() {
-            f if f.is_en_passant() => counter.en_passant += 1,
-            f if f.is_castle() => counter.castles += 1,
-            f if f.is_promotion() => counter.promotions += 1,
-            _ => {}
-        }
-        if board.cur_state().captured_piece.is_some() {
-            counter.captures += 1;
-        }
-        if board.in_check(&board.side_to_move()) {
-            counter.checks += 1;
-        }
-        board.undo_move(&mov, false).expect("Valid undo move");
-        results.push((mov, counter));
-    }
-    results
-}
+// fn perft(depth: u8, board: &mut Board) -> Vec<(Move, MoveCounter)> {
+//     if depth == 0 {
+//         return vec![];
+//     }
+//     let mut results = Vec::new();
+//     let moves = vec![];
+//     for idx in 0..moves.len() {
+//         let mut counter = MoveCounter::default();
+//         let mov = moves[idx];
+//         board.make_move(&mov, false, false).expect("Valid move");
+//         counter += perft_depth(depth - 1, board);
+//         match mov.flag() {
+//             f if f.is_en_passant() => counter.en_passant += 1,
+//             f if f.is_castle() => counter.castles += 1,
+//             f if f.is_promotion() => counter.promotions += 1,
+//             _ => {}
+//         }
+//         if board.cur_state().captured_piece.is_some() {
+//             counter.captures += 1;
+//         }
+//         if board.in_check(&board.side_to_move()) {
+//             counter.checks += 1;
+//         }
+//         board.undo_move(&mov, false).expect("Valid undo move");
+//         results.push((mov, counter));
+//     }
+//     results
+// }
 
-fn perft_depth(depth: u8, board: &mut Board) -> MoveCounter {
-    if depth == 0 {
-        return MoveCounter {
-            count: 1,
-            ..Default::default()
-        };
-    }
-    let mut counter = MoveCounter::default();
+// fn perft_depth(depth: u8, board: &mut Board) -> MoveCounter {
+//     if depth == 0 {
+//         return MoveCounter {
+//             count: 1,
+//             ..Default::default()
+//         };
+//     }
+//     let mut counter = MoveCounter::default();
 
-    let moves = MoveGeneration::generate_all_moves(&board);
-    for mov in moves {
-        board.make_move(&mov, false, false).expect("Valid move");
-        counter += perft_depth(depth - 1, board);
-        match mov.flag() {
-            f if f.is_en_passant() => counter.en_passant += 1,
-            f if f.is_castle() => counter.castles += 1,
-            f if f.is_promotion() => counter.promotions += 1,
-            _ => {}
-        }
-        if board.cur_state().captured_piece.is_some() {
-            counter.captures += 1;
-        }
-        if board.in_check(&board.side_to_move()) {
-            counter.checks += 1;
-        }
-        board.undo_move(&mov, false).expect("Valid undo move");
-    }
-    counter
-}
+//     let moves = vec![];
+//     for mov in moves {
+//         board.make_move(&mov, false, false).expect("Valid move");
+//         counter += perft_depth(depth - 1, board);
+//         match mov.flag() {
+//             f if f.is_en_passant() => counter.en_passant += 1,
+//             f if f.is_castle() => counter.castles += 1,
+//             f if f.is_promotion() => counter.promotions += 1,
+//             _ => {}
+//         }
+//         if board.cur_state().captured_piece.is_some() {
+//             counter.captures += 1;
+//         }
+//         if board.in_check(&board.side_to_move()) {
+//             counter.checks += 1;
+//         }
+//         board.undo_move(&mov, false).expect("Valid undo move");
+//     }
+//     counter
+// }
 
 #[derive(Debug, Clone, Copy)]
 struct MoveCounter {
