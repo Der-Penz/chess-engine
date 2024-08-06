60f7356203211b933e8a777b7091bc995c7c323b
DerPenz <mike2407@t-online.de>
2024-08-06 15:11:00 +0200

    better perft display

--- src/bin/perft.rs
+++ src/bin/perft.rs
@@ -41,7 +41,10 @@ fn main() {
         Board::default()
     };
 
+    //get start time
+    let start = std::time::Instant::now();
     let perft_result = perft(depth, &mut board);
+    let elapsed = start.elapsed();
     match mode {
         PerftMode::NodeCount => {
             let node_count = perft_result
@@ -57,6 +60,19 @@ fn main() {
         PerftMode::DivideDetailed => {
             for (mov, counter) in perft_result.iter() {
                 println!("{}: {}", mov, counter);
+            }
+
+            let total = perft_result
+                .iter()
+                .map(|x| x.1)
+                .reduce(|acc, counter| acc + counter);
+            if let Some(total) = total {
+                println!();
+                println!("Sum:  {}", total);
+                let node_count = total.count;
+                let nodes_per_second = node_count as f64 / elapsed.as_secs_f64();
+                println!("Time taken: {:.3?} s", elapsed.as_secs());
+                println!("Nodes per second: {:.*} N/s", 0, nodes_per_second);
             }
         }
     }
@@ -75,6 +91,7 @@ fn perft(depth: u8, board: &mut Board) -> Vec<(Move, MoveCounter)> {
             .expect("Move generation should generate only legal moves");
         let mut counter = MoveCounter::default();
         counter += perft_depth(depth - 1, board);
+        counter.add_details(board, mov);
         board
             .undo_move(mov, false)
             .expect("Undo move should be valid");
@@ -96,6 +113,7 @@ fn perft_depth(depth: u8, board: &mut Board) -> MoveCounter {
             .make_move(mov, false, false)
             .expect("Move generation should generate only legal moves");
         counter += perft_depth(depth - 1, board);
+        counter.add_details(board, mov);
         board
             .undo_move(mov, false)
             .expect("Undo move should be valid");
@@ -171,6 +189,21 @@ impl MoveCounter {
             castles: 0,
             promotions: 0,
             checks: 0,
+        }
+    }
+
+    fn add_details(&mut self, board: &Board, mov: &Move) {
+        if mov.flag().is_castle() {
+            self.castles += 1;
+        }
+        if mov.flag().is_en_passant() {
+            self.en_passant += 1;
+        }
+        if mov.flag().is_promotion() {
+            self.promotions += 1;
+        }
+        if board.cur_state().captured_piece.is_some() {
+            self.captures += 1;
         }
     }
 }
