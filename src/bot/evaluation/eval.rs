4206ebec33d299d16a21f789349749973ea40eb0
DerPenz <mike2407@t-online.de>
2024-09-28 12:04:59 +0200

    new eval function

--- src/bot/evaluation/eval.rs
+++ src/bot/evaluation/eval.rs
@@ -1,11 +1,16 @@
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+
 pub type Eval = i32;
 
 pub const NEG_INF: Eval = i32::MIN + 1;
 pub const POS_INF: Eval = i32::MAX - 1;
 
 pub const DRAW: Eval = 0;
-pub const MATE: Eval = 10000;
+pub const MATE: Eval = 100000;
 
+/// Check if the evaluation score is a mate score.
+/// Mate scores are defined as a score that is greater than or equal to MATE - 255 or less than or equal to -MATE + 255.
 pub fn is_mate_score(score: Eval) -> bool {
     if score == NEG_INF || score == POS_INF {
         return false;
@@ -14,6 +19,9 @@ pub fn is_mate_score(score: Eval) -> bool {
     score.abs() >= MATE - u8::MAX as i32
 }
 
+/// Display the evaluation score in a human-readable format.
+/// Converts mate scores to "mate X" where X is the number of moves to mate.
+/// Converts centipawn scores to "cp X" where X is the centipawn (divided by 100) value.
 pub fn display_eval(eval: Eval) -> String {
     if is_mate_score(eval) {
         let mate_in = (MATE - eval.abs()) as f32 / 2.0;
@@ -25,4 +33,46 @@ pub fn display_eval(eval: Eval) -> String {
     } else {
         format!("cp {}", eval as f64 / 100.0)
     }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct EvalScores {
+    pub endgame_material_start: Eval,
+    pub piece_values: [Eval; 6],
+    pub king_pawn_shield: Eval,
+    pub king_not_castled: Eval,
+    pub bishop_pair: Eval,
+    pub isolated_pawn: Eval,
+    pub doubled_pawn: Eval,
+    pub rook_semi_open_file: Eval,
+    pub rook_open_file: Eval,
+    pub rook_on_closed: Eval,
+    pub rook_on_seventh: Eval,
+    pub rook_connected: Eval,
+}
+
+impl EvalScores {
+    pub fn load_from_file(path: &str) -> Result<Self, ()> {
+        let content = std::fs::read_to_string(path).map_err(|_| ())?;
+        let scores: EvalScores = serde_json::from_str(&content).map_err(|_| ())?;
+        Ok(scores)
+    }
+}
+
+fn read_start_eval_score() -> EvalScores {
+    let file_path = std::env::var("EVAL_SCORES").unwrap_or_else(|_| "eval_scores.json".to_string());
+    info!("Loading eval scores from file: {}", file_path);
+    //log all env vars
+    for (key, value) in std::env::vars() {
+        info!(
+            "
+        key: {} value: {}",
+            key, value
+        );
+    }
+    EvalScores::load_from_file(&file_path).expect("Missing eval scores file")
+}
+
+lazy_static! {
+    pub static ref EVAL_SCORES: EvalScores = read_start_eval_score();
 }
--- src/bot/evaluation/mod.rs
+++ src/bot/evaluation/mod.rs
@@ -1,28 +1,244 @@
 pub mod eval;
 mod piece_square_table;
-use eval::Eval;
+use eval::{Eval, EVAL_SCORES};
 use piece_square_table::read_p_sq_table;
 
-use crate::game::{bit_manipulation::iter_set_bits, Color, PieceType, Square};
+use crate::game::{
+    bit_manipulation::{
+        constants::{A_FILE, NOT_A_FILE, NOT_H_FILE},
+        iter_set_bits,
+    },
+    board::{
+        bit_board::BitBoard,
+        move_gen::{
+            attacks::direction_mask::{ALIGN_MASKS, CONNECTION_MASK},
+            attacks_rook,
+        },
+    },
+    Color, Piece, PieceType, Square,
+};
 
 use super::Board;
 
 /// Evaluates the board state and returns a score in the perspective of the side to move.
 pub fn evaluate_board(board: &Board) -> Eval {
-    let mut score = 0;
+    let mut ally_score = 0;
+    let mut enemy_score = 0;
 
-    let color = board.side_to_move();
+    let ally = board.side_to_move();
+    let enemy = ally.opposite();
 
-    score += evaluate_pieces(board);
+    let material_info_ally = MaterialInfo::from_board(board, ally);
+    let material_info_enemy = MaterialInfo::from_board(board, enemy);
 
-    score * color.perspective() as Eval
+    ally_score += material_info_ally.total_material;
+    enemy_score += material_info_enemy.total_material;
+
+    // score += evaluate_pieces(board);
+
+    //bishop pair bonus
+    {
+        if material_info_ally.num_pieces[PieceType::Bishop as usize] == 2 {
+            let mut positions = board.get_piece_positions(Piece::new(PieceType::Bishop, ally));
+            let first = positions.next();
+            let second = positions.next();
+            if first
+                .zip(second)
+                .is_some_and(|(a, b)| a.color() != b.color())
+            {
+                ally_score += EVAL_SCORES.bishop_pair;
+            }
+        }
+        if material_info_enemy.num_pieces[PieceType::Bishop as usize] == 2 {
+            let mut positions = board.get_piece_positions(Piece::new(PieceType::Bishop, enemy));
+            let first = positions.next();
+            let second = positions.next();
+            if first
+                .zip(second)
+                .is_some_and(|(a, b)| a.color() != b.color())
+            {
+                enemy_score += EVAL_SCORES.bishop_pair;
+            }
+        }
+    }
+
+    //king pawn shield
+    {
+        let king_sq = board.get_king_pos(ally);
+        let pawns = board.get_bb_pieces()[ally][PieceType::Pawn];
+        let shield = PAWN_SHIELDS[ally][king_sq];
+        ally_score += (pawns & shield).count_ones() as Eval * EVAL_SCORES.king_pawn_shield;
+        let king_sq = board.get_king_pos(enemy);
+        let pawns = board.get_bb_pieces()[enemy][PieceType::Pawn];
+        let shield = PAWN_SHIELDS[enemy][king_sq];
+        enemy_score += (pawns & shield).count_ones() as Eval * EVAL_SCORES.king_pawn_shield;
+    }
+
+    //king not castled penalty
+    {
+        if board.cur_state().castling_rights.has_any(ally) {
+            ally_score += EVAL_SCORES.king_not_castled;
+        }
+        if board.cur_state().castling_rights.has_any(enemy) {
+            enemy_score += EVAL_SCORES.king_not_castled;
+        }
+    }
+
+    //double pawn / isolated pawn
+    {
+        let ally_pawns = *board.get_bb_pieces()[ally][PieceType::Pawn];
+        let enemy_pawns = *board.get_bb_pieces()[enemy][PieceType::Pawn];
+        for i in 0..8 {
+            //double pawn
+            let mask = A_FILE << i;
+            ally_score +=
+                ((ally_pawns & mask).count_ones().max(1) - 1) as Eval * EVAL_SCORES.doubled_pawn;
+            enemy_score +=
+                ((enemy_pawns & mask).count_ones().max(1) - 1) as Eval * EVAL_SCORES.doubled_pawn;
+
+            //isolated pawn
+            let mask_left = A_FILE << (i - 1).max(0);
+            let mask_right = A_FILE << (i + 1).min(7);
+
+            if (ally_pawns & mask) != 0 {
+                let pawns_left = (ally_pawns & mask_left).count_ones();
+                let pawns_right = (ally_pawns & mask_right).count_ones();
+                if pawns_left == 0 && pawns_right == 0 {
+                    ally_score += EVAL_SCORES.isolated_pawn;
+                }
+            }
+            if (enemy_pawns & mask) != 0 {
+                let pawns_left = (enemy_pawns & mask_left).count_ones();
+                let pawns_right = (enemy_pawns & mask_right).count_ones();
+                if pawns_left == 0 && pawns_right == 0 {
+                    enemy_score += EVAL_SCORES.isolated_pawn;
+                }
+            }
+        }
+    }
+
+    //rook file open / closed / semi open
+    //rook on 7th
+    {
+        let ally_rooks = board.get_bb_pieces()[ally][PieceType::Rook];
+        for sq in ally_rooks.get_occupied() {
+            let mask = A_FILE << sq.file();
+            let pawns = *board.get_bb_pieces()[ally][PieceType::Pawn];
+            let pawns_enemy = *board.get_bb_pieces()[enemy][PieceType::Pawn];
+            let pawns_on_file = (pawns | pawns_enemy) & mask;
+            if pawns_on_file.count_ones() == 0 {
+                ally_score += EVAL_SCORES.rook_open_file;
+            } else if (mask & pawns) == 0 {
+                ally_score += EVAL_SCORES.rook_semi_open_file;
+            } else {
+                ally_score += EVAL_SCORES.rook_on_closed;
+            }
+
+            let seventh_rank = match ally {
+                Color::White => 6,
+                Color::Black => 1,
+            };
+
+            if sq.rank() == seventh_rank {
+                ally_score += EVAL_SCORES.rook_on_seventh;
+            }
+
+            if attacks_rook(sq, *ally_rooks, 0) != 0 {
+                ally_score += EVAL_SCORES.rook_connected;
+            }
+        }
+
+        let enemy_rooks = board.get_bb_pieces()[enemy][PieceType::Rook];
+        for sq in enemy_rooks.get_occupied() {
+            let mask = A_FILE << sq.file();
+            let pawns = *board.get_bb_pieces()[enemy][PieceType::Pawn];
+            let pawns_enemy = *board.get_bb_pieces()[ally][PieceType::Pawn];
+            let pawns_on_file = (pawns | pawns_enemy) & mask;
+            if pawns_on_file.count_ones() == 0 {
+                enemy_score += EVAL_SCORES.rook_open_file;
+            } else if (mask & pawns) == 0 {
+                enemy_score += EVAL_SCORES.rook_semi_open_file;
+            } else {
+                enemy_score += EVAL_SCORES.rook_on_closed;
+            }
+
+            let seventh_rank = match enemy {
+                Color::White => 6,
+                Color::Black => 1,
+            };
+
+            if sq.rank() == seventh_rank {
+                enemy_score += EVAL_SCORES.rook_on_seventh;
+            }
+
+            if attacks_rook(sq, *enemy_rooks, 0) != 0 {
+                enemy_score += EVAL_SCORES.rook_connected;
+            }
+        }
+    }
+
+    let score = ally_score - enemy_score;
+    score * ally.perspective() as Eval
 }
 
 ///  Rook + Bishop + Knight + Queen:
 const END_GAME_MATERIAL_START: Eval = 2050;
 
+const PAWN_SHIELDS: [[u64; 64]; 2] = init_pawn_shields();
+
+const fn init_pawn_shields() -> [[u64; 64]; 2] {
+    let mut shields = [[0; 64]; 2];
+    let mut i = 0;
+    while i < 64 {
+        let cur = 1 << i;
+        let mut shield = 0;
+        shield |= (cur << 7) & NOT_H_FILE;
+        shield |= cur << 8;
+        shield |= (cur << 9) & NOT_A_FILE;
+
+        shields[0][i] = shield;
+        let mut shield = 0;
+        shield |= (cur >> 7) & NOT_A_FILE;
+        shield |= cur >> 8;
+        shield |= (cur >> 9) & NOT_H_FILE;
+
+        shields[1][i] = shield;
+
+        i += 1;
+    }
+    shields
+}
+
 fn calc_endgame_factor(sum: i32) -> f32 {
     1_f32 - 1_f32.min(sum as f32 / END_GAME_MATERIAL_START as f32)
+}
+
+struct MaterialInfo {
+    num_pieces: [u8; 6],
+    material_score: [Eval; 6],
+    total_material: Eval,
+}
+
+impl MaterialInfo {
+    fn from_board(board: &Board, color: Color) -> MaterialInfo {
+        let mut num_pieces = [0; 6];
+        let mut material_score = [0; 6];
+        let mut total_material = 0;
+
+        for piece in PieceType::iter() {
+            let bb = board.get_bb_pieces()[color][piece];
+            num_pieces[piece as usize] = bb.count_ones() as u8;
+            let material = EVAL_SCORES.piece_values[piece] * num_pieces[piece as usize] as Eval;
+            material_score[piece as usize] = material;
+            total_material += material;
+        }
+
+        MaterialInfo {
+            num_pieces,
+            material_score,
+            total_material,
+        }
+    }
 }
 
 #[inline(always)]
